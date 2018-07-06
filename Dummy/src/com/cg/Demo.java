ffe1c4fa16bd8ea00484de5f8f9b56d63f0572c7
aman <aman@DIN66007828.corp.capgemini.com>
2018-07-06 17:48:33 +0530

    v2

--- Dummy/src/com/cg/Demo.java
+++ Dummy/src/com/cg/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Github Demo");
+		System.out.println("Github Demo.............");
+		System.out.println("connect again");
 	}
 
 }
