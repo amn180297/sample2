abaf2bf595c68770d0158419ea80229bc0f2d2a0
aman <aman@DIN66007828.corp.capgemini.com>
2018-08-10 09:59:41 +0530

    v3

--- sample/src/com/cg/sample/SampleCode.java
+++ sample/src/com/cg/sample/SampleCode.java
@@ -5,6 +5,7 @@ public class SampleCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world v2");
+System.out.println("Github");
 	}
 
 }
